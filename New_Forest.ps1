e49841699c489655bdd07e48c942ebf3f5bd7046
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-02-09 13:54:40 +0300

    Update New_Forest.ps1

--- New_Forest.ps1
+++ New_Forest.ps1
@@ -1,15 +1,16 @@
-#Установка ролей
+#Add Roles and Features
 Import-Module ServerManager
 Add-WindowsFeature –Name AD-Domain-Services –IncludeAllSubFeature –IncludeManagementTools
 
-#Имя Домена
+#Domain Name
 $Domain_Name = 'domain_1.local'
 
-$Пароль DSRM
+#DSRM Password
 $DSRM_password = 'P@$$w0rd'
 
 $Netbios = $Domain_Name.ToUpper().Split(".")[0]
-    
+
+#Create new forest
 Import-Module ADDSDeployment
 Install-ADDSForest `
     -CreateDnsDelegation:$false `
